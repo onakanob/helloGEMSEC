5f03b55790885c8511850b3eeb3a8de3718129ff
Oliver <onakanob@gmail.com>
2018-05-07 17:31:26 -0700

    small changes

--- entropy/EntropyDemo.m
+++ entropy/EntropyDemo.m
@@ -29,3 +29,15 @@ subplot(5,2,8), imhist(I4), title(entropy(I4));
 I5 = uint8(64.*(ceil(4.*rand(64, 64))-1));
 subplot(5,2,9), imshow(I5);
 subplot(5,2,10), imhist(I5), title(entropy(I5));
+
+%% Signal content stuff
+I_signal = uint8(zeros(256,256));
+for i = 1:256
+    I_signal(:,i) = i-1;
+end
+I_signal = [I_signal uint8(100*ones(256,256))];
+
+I_reduced = uint8(zeros(256,256));
+for i = 1:256
+    I_reduced(:,i) = mod((i-1),128);
+end
